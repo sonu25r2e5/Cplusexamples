03b5e991fd30eaa6b188efb2054445dcdb3a1a37
sonu25r2e5 <sk7018059@gmail.com>
2025-03-04 06:30:24 +0545

    RealExamples

--- RealExam.cpp
+++ RealExam.cpp
@@ -2,6 +2,20 @@
 using namespace std;
  
 int main() {
- cout << "Hello World!";
-    return 0;
+int items ; 
+double cost_per_item; 
+double total_cost; 
+char currency = '$';
+
+items = 5;
+cost_per_item = 8.5;
+total_cost = items * cost_per_item;;
+
+// print variables. 
+cout << "Items: " << items << endl;
+cout << "Cost per item: " << currency << cost_per_item << endl;
+cout << "Total cost: " << currency << total_cost << endl;
+
+
+
 }
